582fdfaa1d8db7821c4f3ea14e15a8fc43789d79
sh1mj1 <7wlgns@gmail.com>
2024-09-04 11:17:08 +0900

    feat(NetworkMarsPhotosRepository)

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,7 +1,12 @@
 package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApi
 
 interface MarsPhotosRepository {
     suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotosRepository : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = MarsApi.retrofitService.getPhotos()
 }
